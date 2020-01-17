5b01759b5c4851314aa3d949e5258ac53a26840f
mhr <2247469r@gla.ac.uk>
2020-01-17 13:12:15 +0000

    Minor change to remove unecessary comments

--- accelerometer/accelerometer.py
+++ accelerometer/accelerometer.py
@@ -6,14 +6,5 @@ i2c = I2C(1, scl=Pin(14), sda=Pin(13), freq=4000)
 a_sensor = lis3dh.LIS3DH_I2C(i2c)
 
 while True:
-    """
-    hspi = SPI(1, 1000, sck=Pin(14), mosi=Pin(13), miso=Pin(12))
-    cs = Pin(2, Pin.OUT)
-
-    accel_sensor = lis3dh.LIS3DH_SPI(hspi, cs)
-    print(accel_sensor.acceleration)
-
-    time.sleep_ms(1)
-    """
-
     print(a_sensor.acceleration, end='\r')
+
