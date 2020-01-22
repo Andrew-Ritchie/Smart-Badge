6a234a29adaf85e516e5e6197e1e99c7b56665f2
Ashwin-MJ <ashwinmj@gmail.com>
2020-01-22 11:34:44 +0000

    Test code to display "Hello World" with LittlevGL

--- dev/display_test.py
+++ dev/display_test.py
@@ -1,11 +1,29 @@
-from ili9341 import ILI9341
-import time
+# RELIES ON LITTLEVGL FIRMWARE BEING FLASHED ON ESP32
 
-display = ILI9341()
-display.erase()
+# Test program to initialise ILI9341 display and register the driver
+import lvgl as lv
+from micropython import const
+from ili9341 import ili9341
 
-for i in [0, 90, 180, 270]:
-    display.erase()
-    display.set_rotation(i)
-    display.print("Hello {}".format(i))
-    time.sleep_ms(1000)
+# Initialise LittlevGL
+lv.init()
+
+# Initialise and register the display
+# Registering is handled automatically by this contructor
+# Change 'rot' parameter to edit rotation
+disp = ili9341(miso=5, mosi=18, clk=19, cs=13,
+               dc=12, rst=4, backlight=2, width=128,
+               height=160, backlight_on=1, power=14,
+               power_on=1, rot=const(0x04), invert=True)
+
+# Create a screen
+scr = lv.obj()
+
+# Create a button on the screen and set label
+btn = lv.btn(scr)
+btn.align(lv.scr_act(), lv.ALIGN.CENTER, 0, 0)
+label = lv.label(btn)
+label.set_text("Hello World!")
+
+# Load the screen at the end
+lv.scr_load(scr)
