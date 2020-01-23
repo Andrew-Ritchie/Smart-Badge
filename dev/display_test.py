71a75c4aed929faaa6bcb28ae319a5c2f5bd2fe4
mhr <2247469r@gla.ac.uk>
2020-01-23 15:42:41 +0000

    Added code to print accelerometer and button state to display - Each acceleration channel is placed in a text field - The state of the button connected to pin 34 is printed to screen

--- dev/display_test.py
+++ dev/display_test.py
@@ -5,7 +5,16 @@ import lvgl as lv
 from micropython import const
 from ili9341 import ili9341
 from widgets import Button, TextArea, Line
+from machine import Pin, I2C
 import time
+import lis3dh
+
+# Set up button input
+b1 = Pin(34, mode=Pin.IN)
+
+# Set up I2C bus for accelerometer
+i2c = I2C(1, scl=Pin(14), sda=Pin(13), freq=4000)
+a_sensor = lis3dh.LIS3DH_I2C(i2c)
 
 # Initialise LittlevGL
 lv.init()
@@ -31,15 +40,26 @@ max_width = scr.get_width()-10
 # btn2 = Button(scr, 5, 35, width=max_width, height=20)
 # btn2.set_text("Option 2")
 
-# ta = TextArea(scr, 5, 65, max_width, 30)
-# ta.set_text_content("Hello World!")
+text_button = TextArea(scr, 5, 65, max_width, 30)
+text_accelx = TextArea(scr, 5, 65, max_width, 30)
+text_accely = TextArea(scr, 5, 65, max_width, 30)
+text_accelz = TextArea(scr, 5, 65, max_width, 30)
 
-line = Line(scr, 10, 10)
-style = line.create_style(colour=(0xff, 0x00, 0x00), width=1, rounded=1)
-line.set_custom_style(style)
-line.set_line_points([{"x": 5, "y": 5},
-                      {"x": 70, "y": 70},
-                      {"x": 100, "y": 10},
-                      {"x": 5, "y": 5}])
+#line = Line(scr, 10, 10)
+#style = line.create_style(colour=(0xff, 0x00, 0x00), width=1, rounded=1)
+#line.set_custom_style(style)
+#line.set_line_points([{"x": 5, "y": 5},
+#                      {"x": 70, "y": 70},
+#                      {"x": 100, "y": 10},
+#                      {"x": 5, "y": 5}])
 
-lv.scr_load(scr)
+while True:
+    text_button.set_text_content("pressed" if b1.value()==1 else "released")
+    x,y,z = a_sensor.acceleration
+    text_accelx.set_text_content(str(x))
+    text_accely.set_text_content(str(y))
+    text_accelz.set_text_content(str(z))
+
+    lv.scr_load(scr)
+    time.sleep_ms(1)
+
