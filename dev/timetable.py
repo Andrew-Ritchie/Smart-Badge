773881e199cd88f2f288099e3303709441264bb2
Andrew Ritchie <andrew@glaroam2-143-165.wireless.gla.ac.uk>
2020-02-10 15:19:04 +0000

    t

---
+++ dev/timetable.py
@@ -0,0 +1,14 @@
+#import display as dis
+import settings as s
+import widgets as w
+
+class Timetable(s.Settings):
+    def __init__(self):
+        super().__init__("settings")
+
+
+
+
+
+
+w.print("helloooo")
