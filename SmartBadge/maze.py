f31976892e3523375f3e8a97c965e643e8ce152f
Ashwin Maliampurakal <45148261+Ashwin-MJ@users.noreply.github.com>
2020-03-11 12:02:40 +0000

    45 menu for apps (#48)
    * #38-#39-fixed angles
    
    * #42 Initial maze structure in main
    
    * #45 Added menu for Pong
    
    * #45 Menu for maze game and high scores added
    
    * #45 Updated version of pong with label for scores
    
    Co-authored-by: Conor Begley <conorbegley@outlook.com>

--- SmartBadge/maze.py
+++ SmartBadge/maze.py
@@ -1,30 +1,113 @@
 import lvgl as lv
-from lib.screen.widgets import *
-import lib.app as app
+from lib.app import App, GameApp
 import lib.game.game as g
+from lib.screen.widgets import Button, Label
+from settings import HighScores
 
 
-class MazeApp(app.GameApp):
+class MazeMenuApp(App):
 
-    def __init__(self, disp):
+    def __init__(self, disp, buttons, tim):
+        super().__init__(name="Maze Menu", display=disp, buttons=buttons, timer=tim,
+                         btn_left=self.btn_left,
+                         btn_right=self.btn_right,
+                         btn_b=self.btn_b,
+                         btn_y=self.btn_y)
+        self.set_title("Maze Game", font_size=28)
 
-        super().__init__("Maze", disp)
+        cont = self.get_cont()
 
-        self.add_sprite("ball", 10, 10, 4, 2, "BALL")
-        #border walls
-        self.add_sprite("WALL", 0, 0, 32, 1)
-        self.add_sprite("WALL", 1, 0, 1, 32)
-        self.add_sprite("WALL", 0, 31, 32, 1)
-        #wall with exit
-        self.add_sprite("WALL", 1, 31, 1, 17)
-        self.add_sprite("WALL", 16, 31, 1, 15)
+        self.add_item("load_game", Button(
+            cont.lv_obj, text="Load Game", app=MazeGameApp), selectable=True)
+        self.add_item("high_scores", Button(
+            cont.lv_obj, text="High Scores", app=MazeScoresApp), selectable=True)
 
-        #internal walls
+        self.load_screen()
 
-        self.add_sprite("WALL", 3, 1, 1, 11)
-        self.add_sprite("WALL", 3, 17, 1, 6)
-        self.add_sprite("WALL", 3, 25, 1, 11)
-        self.add_sprite("WALL", 5, 20, 1, 6)
-        self.add_sprite("WALL", 7, 5, 1, 2)
-        self.add_sprite("WALL", 15, 31, 1, 17)
+    def btn_left(self, x):
+        lv.group_focus_prev(self.group)
 
+    def btn_right(self, x):
+        lv.group_focus_next(self.group)
+
+    def btn_b(self, x):
+        focused = lv.group_get_focused(self.group)
+        app = self.item_ids[id(focused)].app_name
+        ac_app = app(self.disp, self.buttons, self.tim)
+
+    def btn_y(self, x):
+        from main_menu import MainMenuApp
+        MainMenuApp(self.disp, self.buttons, self.tim)
+
+
+class MazeScoresApp(App):
+
+    def __init__(self, disp, buttons, tim):
+        super().__init__(name="Maze Game Scores", display=disp, buttons=buttons, timer=tim,
+                         btn_y=self.btn_y)
+        self.set_title("High Scores", font_size=28)
+
+        cont = self.get_cont()
+
+        first, second, third = self.get_high_scores()
+
+        self.add_item("first", Label(cont.lv_obj, first))
+        self.add_item("second", Label(cont.lv_obj, second))
+        self.add_item("third", Label(cont.lv_obj, third))
+
+        self.load_screen()
+
+    def get_high_scores(self):
+        scores = HighScores("high_scores.json", "maze")
+        return scores.get_top_three().split(" ")
+
+    def btn_y(self, x):
+        MazeMenuApp(self.disp, self.buttons, self.tim)
+
+
+class MazeGameApp(GameApp):
+
+    def __init__(self, disp, buttons, tim):
+
+        super().__init__("Maze", display=disp, buttons=buttons,  debug=False, roll_over=False, border=False, kill=False,
+                         btn_left=self.btn_left,
+                         btn_right=self.btn_right,
+                         btn_up=self.btn_up,
+                         btn_down=self.btn_down,
+                         btn_y=self.btn_y)
+
+        self.tim = tim
+        self.add_sprite("ball", 10, 10, 2, 2, "BALL")
+        # border walls
+        self.add_sprite("t_wall", 0, 0, 32, 1, "WALL")
+        self.add_sprite("l_wall", 0, 0, 1, 32, "WALL")
+        self.add_sprite("b_wall", 0, 31, 32, 1, "WALL")
+        # wall with exit
+        self.add_sprite("r_wall1", 31, 0, 1, 17, "WALL")
+        self.add_sprite("r_wall2", 31, 20, 1, 13, "WALL")
+
+        self.load_screen()
+
+        # internal walls
+
+        # self.add_sprite("WALL", 3, 1, 1, 11)
+        # self.add_sprite("WALL", 3, 17, 1, 6)
+        # self.add_sprite("WALL", 3, 25, 1, 11)
+        # self.add_sprite("WALL", 5, 20, 1, 6)
+        # self.add_sprite("WALL", 7, 5, 1, 2)
+        # self.add_sprite("WALL", 15, 31, 1, 17)
+
+    def btn_up(self, x):
+        self.move_sprite("ball", 0, -1)
+
+    def btn_down(self, x):
+        self.move_sprite("ball", 0, 1)
+
+    def btn_left(self, x):
+        self.move_sprite("ball", -1, 0)
+
+    def btn_right(self, x):
+        self.move_sprite("ball", 1, 0)
+
+    def btn_y(self, x):
+        MazeMenuApp(self.disp, self.buttons, self.tim)
--- SmartBadge/test_maze.py
+++ SmartBadge/test_maze.py
@@ -3,30 +3,16 @@ from lib.screen.display import Display
 from maze import MazeApp
 import time
 import lib.ext.sensors as sensors
+from machine import Timer
+
 # Initialise LittlevGL -- for display
 lv.init()
 
 # Initialise and register the display
 disp = Display()
 
-maze_app = MazeApp(disp)
+buttons = sensors.Buttons(up=27, down=33, left=25,
+                          right=12, a=35, b=26, x=34, y=32)
 
-buttons = sensors.Buttons(up=27, down=33, left=25, right=12, a=35, b=26, x=34, y=32)
-
-def go_up(x):
-     maze_app.move_sprite("ball",0,1)
-def go_down(x):
-     maze_app.move_sprite("ball",0,-1)
-def go_left(x):
-     maze_app.move_sprite("ball",1,0)
-def go_right(x):
-     maze_app.move_sprite("ball",-1,0)
-
-buttons.left.set_callback_edge(go_up)
-buttons.up.set_callback_edge(go_down)
-buttons.down.set_callback_edge(go_left)
-buttons.right.set_callback_edge(go_right)
-
-while True:
-    print("\n\n")
-    time.sleep_ms(100)
+tim = Timer(-1)
+MazeApp(disp, buttons, tim)
