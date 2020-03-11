6ccc07e625a2b00171ddd0da76bafe6e3606b0ab
Ashwin-MJ <ashwinmj@gmail.com>
2020-03-11 09:30:49 +0000

    #47 Contact and URL QR code generator

---
+++ device_setup/qrcodegen.py
@@ -0,0 +1,24 @@
+import pyqrcode
+from segno import helpers
+
+
+class URLGenerator():
+
+    def __init__(self, url, filename):
+        url = pyqrcode.create(url)
+        url.svg(filename, scale=8)
+
+
+class ContactGenerator():
+
+    def __init__(self, name, displayname, email, phone):
+        qr = helpers.make_vcard(
+            name=name, displayname=displayname, email=email, phone=phone)
+        qr.save("{}.png".format(name), scale=4)
+
+
+github = URLGenerator(
+    "https://github.com/Andrew-Ritchie/Smart-Badge", "SmartBadgeGithub.svg")
+
+ashwin = ContactGenerator("Ashwin;Jaison;Maliampurakal",
+                          "Ashwin Maliampurakal", "2249314m@student.gla.ac.uk", "123456")
