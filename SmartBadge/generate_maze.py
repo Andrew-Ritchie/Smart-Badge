96bdaa0ad42a4e3f9c356ded2a35f879752505ae
Conor Begley <conorbegley@outlook.com>
2020-03-12 14:05:54 +0000

    Updated maze to draw using generator. Issue is present with different aspect ratios, causing lines to overlap

--- SmartBadge/generate_maze.py
+++ SmartBadge/generate_maze.py
@@ -8,18 +8,18 @@ grid = [[1,1,0,0,1,1,1,1,0],
         [1,0,0,0,0,0,1,1,0],
         [1,0,0,0,0,0,0,0,0]]
 
-results = [(0,0,2,1),
-           (0,4,4,1),
-           (1,0,1,4),
-           (1,4,1,3),
-           (1,6,2,1),  
-           (3,1,2,1),
-           (3,5,3,1),
-           (4,6,2,1),
-           (6,0,2,1), 
-           (6,4,4,1),
-           (7,0,1,2), 
-           (7,6,2,1),
+results = [[0,0,2,1],
+           [4,0,4,1],
+           [0,1,1,4],
+           [4,1,1,3],
+           [6,1,2,1],  
+           [1,3,2,1],
+           [5,3,3,1],
+           [6,4,2,1],
+           [0,6,2,1], 
+           [4,6,4,1],
+           [0,7,1,2], 
+           [6,7,2,1],
            ]
 def generate_wall_list(grid):
     y = len(grid)
@@ -33,7 +33,7 @@ def generate_wall_list(grid):
     
     for i in range (0,y):
         for ran in tracker[i]:
-            current_wall = [i,ran[0],0,1] 
+            current_wall = [ran[0],i,0,1] 
             wall = False
             wall_size = 0
             for j in range (ran[0],ran[1]):
@@ -54,13 +54,19 @@ def generate_wall_list(grid):
                         wall = False
                         walls.append(tuple(current_wall))
                     wall_size = 0
-                    current_wall = [i, j+1, 0, 1]
+                    current_wall = [j+1, i, 0, 1]
         
             if wall == True:
-                walls.append(tuple(current_wall))
+                walls.append((current_wall))
     return walls
 
-w = generate_wall_list(grid)
+def reformat_to_line(walls):
+    line_points = []
+    for wall in walls:
+        p1 = (wall[0],wall[1])
+        p2 = (wall[0]+wall[2]-1,wall[1]+wall[3]-1)
+        line_points.append([p1,p2])
+    return line_points
 
-print(w)
-print(len(w))
+w = generate_wall_list(grid)
+print(reformat_to_line(w))
