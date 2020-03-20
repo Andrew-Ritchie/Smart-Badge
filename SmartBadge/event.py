2012c37297365c1dfc6f08f59e2f9b2182e4c405
andrew-ritchie <2253409r@student.gla.ac.uk>
2020-03-20 14:58:18 +0000

    changes

--- SmartBadge/event.py
+++ SmartBadge/event.py
@@ -3,7 +3,7 @@ from lib.screen.widgets import *
 from timedata import TimeData
 from lib.app import App
 from main_menu import MainMenuApp
- 
+
 
 class EventApp(App):
 
@@ -35,16 +35,20 @@ class EventApp(App):
 
 
     def btn_left(self, x):
-        self.slot -= 1
-        focused = lv.group_get_focused(self.group)
-        app = self.item_ids[id(focused)].app_name
-        ac_app = app(self.disp, self.buttons, self.tim, self.subject, self.slot)        
+        slot_num = self.slot -= 1
+        time_data = TimeData("timedata.json", self.subject, slot_num)
+        return time_data.get_time_data().split(" ")
+        # focused = lv.group_get_focused(self.group)
+        # app = self.item_ids[id(focused)].app_name
+        # ac_app = app(self.disp, self.buttons, self.tim, self.subject, self.slot)
 
     def btn_right(self, x):
-        self.slot += 1
-        focused = lv.group_get_focused(self.group)
-        app = self.item_ids[id(focused)].app_name
-        ac_app = app(self.disp, self.buttons, self.tim, self.subject, self.slot)
+        slot_num = self.slot += 1
+        time_data = TimeData("timedata.json", self.subject, slot_num)
+        return time_data.get_time_data().split(" ")
+        # focused = lv.group_get_focused(self.group)
+        # app = self.item_ids[id(focused)].app_name
+        # ac_app = app(self.disp, self.buttons, self.tim, self.subject, self.slot)
 
     def btn_b(self, x):
         focused = lv.group_get_focused(self.group)
@@ -52,6 +56,6 @@ class EventApp(App):
         ac_app = app(self.disp, self.buttons, self.tim)
 
     def get_data(self):
-        slot_num = self.slot + 1
+        # slot_num = self.slot + 1
         time_data = TimeData("timedata.json", self.subject, slot_num)
         return time_data.get_time_data().split(" ")
