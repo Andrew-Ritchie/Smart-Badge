c283bb48c33578804f5527038dff32602bb86d9f
Conor Begley <conorbegley@outlook.com>
2020-03-20 15:13:15 +0000

    Ashwin is complaing about using Vim

--- SmartBadge/event.py
+++ SmartBadge/event.py
@@ -11,7 +11,7 @@ class EventApp(App):
         super().__init__(name="Name", display=disp, buttons=buttons, timer=tim,
                          btn_left=self.btn_left,
                          btn_right=self.btn_right,
-                         btn_b=self.btn_b)
+                         )
 
         self.subject = "EEE"
         self.slot = slot
@@ -19,7 +19,7 @@ class EventApp(App):
         cont.set_center()
 
         first, nick, last = self.get_data()
-
+    
         self.add_item("Title", Label(cont.lv_obj, first, font_size=28))
         self.add_item("Time", Label(cont.lv_obj, nick))
         self.add_item("Location", Label(cont.lv_obj, last))
@@ -35,7 +35,7 @@ class EventApp(App):
 
 
     def btn_left(self, x):
-        slot_num = self.slot -= 1
+        slot_num = self.slot - 1
         time_data = TimeData("timedata.json", self.subject, slot_num)
         return time_data.get_time_data().split(" ")
         # focused = lv.group_get_focused(self.group)
@@ -43,17 +43,12 @@ class EventApp(App):
         # ac_app = app(self.disp, self.buttons, self.tim, self.subject, self.slot)
 
     def btn_right(self, x):
-        slot_num = self.slot += 1
+        slot_num = self.slot + 1
         time_data = TimeData("timedata.json", self.subject, slot_num)
         return time_data.get_time_data().split(" ")
         # focused = lv.group_get_focused(self.group)
         # app = self.item_ids[id(focused)].app_name
         # ac_app = app(self.disp, self.buttons, self.tim, self.subject, self.slot)
-
-    def btn_b(self, x):
-        focused = lv.group_get_focused(self.group)
-        app = self.item_ids[id(focused)].app_name
-        ac_app = app(self.disp, self.buttons, self.tim)
 
     def get_data(self):
         # slot_num = self.slot + 1
